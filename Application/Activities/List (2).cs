7569c98179750dfc18ad83d648404b0ccddf5ccf
Zandile Tshabalala <zTshabalala@SINGULAR.CO.ZA>
2022-09-19 13:01:13 +0200

    commit

---
+++ Application/Activities/List (2).cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Domain; 
+
+namespace Application.Activities
+{
+    public class NewBaseType
+    {
+    }
+
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> {}
+
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+        private readonly DataContext _context;
+        public Handler(DataContext context)
+        {
+            this._context = context;
+
+        }
+
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)  
+            {
+                return await _context.Activities.ToListAsync();
+            }
+        }
+    }
+}
